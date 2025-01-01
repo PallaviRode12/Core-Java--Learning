669b5a8bd204633ea4748214d2486614fc8a2940
PallaviRode12 <pallavirode07@gmail.com>
2025-01-01 15:26:43 +0530

    Add files via upload

---
+++ Rev.java
@@ -0,0 +1,16 @@
+class Rev
+{
+    public static void main(String[] args) {
+
+    int no=5248,rev=0,rem;
+    while(no!=0)
+    {
+        rem=no%10;
+        rev=rev*10+rem;
+        no=no/10;
+    }
+
+        System.out.println(rev);
+        
+    }
+}
