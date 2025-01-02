cec681d1c901b20893f7528a27865066bace1e07
PallaviRode12 <pallavirode07@gmail.com>
2025-01-02 17:11:57 +0530

    Add files via upload

---
+++ Stringrev.java
@@ -0,0 +1,18 @@
+ class B {
+    public static void main(String[] args) 
+    {
+        int no=7;
+        for(int i=2;i<=6;i++)
+        {
+            if(no%i==0)
+            {
+                System.out.println("the number is not prime no.");
+            }
+            else{
+                System.out.println("the number is  prime no.");
+            }
+        }
+
+        
+    }
+    }
